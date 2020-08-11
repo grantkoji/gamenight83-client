02e55142d2b7ce4d031af6b5479d0a2755064651
grantkoji <grantkoji@gmail.com>
2020-08-10 21:42:49 -0400

    with changes to UserPageNavBar

---
+++ src/Navbar/UserPageNavbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {connect} from 'react-redux'
+ 
+ const UserPageNavbar = props => {
+
+    const {currentUser, showUser, handleView} = props
+
+     return (
+       <>
+        <div>
+            <button name="photos" onClick={handleView}>Photos</button>
+            <button name="reviews" onClick={handleView}>Reviews</button>
+            <button name="friends" onClick={handleView}>Friends</button>
+            {currentUser.id === showUser 
+            ? <button name="games" onClick={handleView}>Games</button>
+            : <button name='mutualFriends' onClick={handleView}>Mutual Friends</button>}
+            { }
+       </div>
+       </>
+     )
+   
+ }
+ 
+ const mapStateToProps = state => {
+    return {
+      currentUser: state.currentUser,
+      showUser: state.showUser
+    }
+  }
+
+export default connect(mapStateToProps)(UserPageNavbar)
+
