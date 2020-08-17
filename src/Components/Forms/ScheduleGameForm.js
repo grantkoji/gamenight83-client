12f9a85191c245c4fb485555d1c3432726bad7db
grantkoji <grantkoji@gmail.com>
2020-08-17 13:36:13 -0400

    setting up scheduled games

--- src/Components/Forms/ScheduleGameForm.js
+++ src/Components/Forms/ScheduleGameForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as action from '../../modules/actionCreators/actionCreators'
 import {connect} from 'react-redux'
 import { withRouter } from 'react-router-dom'
@@ -45,9 +45,6 @@ const ScheduleGameForm = props => {
       '07': 7,
       '08': 8,
       '09': 9,
-      '10': 10,
-      '11': 11,
-      '12': 12
     }
 
 
@@ -56,43 +53,69 @@ const ScheduleGameForm = props => {
 
   }
 
+
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    let hours = numHash[time.split(':')[0]]
-    let minutes = numHash[time.split(':')[1]]
-    let year = parseInt(date.split('-')[0])
-    let month = numHash[date.split('-')[1]]
-    let day
-    if ( day === '01' || day === '02' || day === '03'|| day === '04'|| day === '05'
-    || day === '06'|| day === '07'|| day === '08'|| day === '09') {
-      day = numHash[date.split('-')[2]]
+
+    if (date === '') {
+        alert('You must set a date to schedule a game')
+    } else if (time === '') {
+      alert('You must set a time to schedule a game')
+    } else if (publicDescription === '') {
+        alert('Please enter notes for players interested in playing')
+    } else if (privateDirections === '') {
+        alert('Please enter notes/directions/zoom links to pass along to players who sign up.')
     } else {
-      day = parseInt([date.split('-')][2])
+      let year = parseInt(date.split('-')[0])
+      let month = monthNumHash[date.split('-')[1]]
+      let dayKey = date.split('-')[2]
+      let day
+      let hoursKey = time.split(':')[0]
+      let hours
+      let minutesKey = time.split(':')[1]
+      let minutes
+      
+      if ( dayKey === '01' || dayKey === '02' || dayKey === '03'|| dayKey === '04'|| dayKey === '05'
+      || dayKey === '06'|| dayKey === '07'|| dayKey === '08'|| dayKey === '09') {
+        day = numHash[dayKey]
+      } else {
+        day = parseInt(dayKey)
+      }
+  
+      if ( hoursKey === '01' || hoursKey === '02' || hoursKey === '03'|| hoursKey === '04'|| hoursKey === '05'
+      || hoursKey === '06'|| hoursKey === '07'|| hoursKey === '08'|| hoursKey === '09') {
+        hours = numHash[hoursKey]
+      } else {
+        hours = parseInt(hoursKey)
+      }
+  
+      if ( minutesKey === '01' || minutesKey === '02' || minutesKey === '03'|| minutesKey === '04'|| minutesKey === '05'
+      || minutesKey === '06'|| minutesKey === '07'|| minutesKey === '08'|| minutesKey === '09') {
+        minutes = numHash[minutesKey]
+      } else {
+        minutes = parseInt(minutesKey)
+      }
+      // setUnix()
+      setUnix(moment([year, month, day, hours, minutes, 0, 0]).unix())
+    
+        
+        
+        
+        console.log('Pub descr', publicDescription)
+        console.log('direction', privateDirections)
+        console.log('privacy',  privacy)
+
+
+        // let createUnix = moment()
+        // setUnix(createUnix)
+        // handleFetchCreatePlayedGame()
     }
-    console.log('hours', hours)
-    console.log('minutes', minutes)
-    console.log('year', year)
-    console.log('month', month)
-    console.log('day', day)
-    console.log('public description', publicDescription)
-    console.log('private directions', privateDirections)
-    console.log('numVacancies', numVacancies)
-    console.log('privacy', privacy)
-
-
-
-    // if (date === '') {
-    //     alert('You must set a date to schedule a game')
-    // } else if (publicDescription === '') {
-    //     alert('Please enter notes for players interested in playing')
-    // } else if (privateDescription === '') {
-    //     alert('Please enter notes/directions/zoom links to pass along to players who sign up.')
-    // } else {
-    //     let createUnix = moment()
-    //     setUnix(createUnix)
-    //     handleFetchCreatePlayedGame()
-    // }
   }
+
+  // useEffect(() => {
+    
+  // }, [unix])
   
     const handleFetchCreatePlayedGame = () => {
       fetch("http://localhost:3001/api/v1/scheduled_games", {
@@ -105,8 +128,8 @@ const ScheduleGameForm = props => {
           game_id: thisGameId,
         //   host_id: currentUser.id, 
           unix, 
-          num_vacancies: numVacancies,
-          status: "scheduled_with_openings",
+          num_vacancies: parseInt(numVacancies),
+          status: "scheduled",
           public_description: publicDescription,
           private_directions: privateDirections,
           privacy
@@ -175,23 +198,23 @@ const ScheduleGameForm = props => {
           <Form className="ui form" onSubmit={handleSubmit}>
             <h3 class="ui dividing header">Game Title: {thisGameTitle}</h3>
             <div className="fields">
-              <div className="four wide field">
-                <label htmlFor="date">Date:</label>
+              <div className="five wide field">
+                <label htmlFor="date">Date</label>
                 <input className='input-schedule-game' type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)}/>
               </div>
               <div className="four wide field">
-                <label htmlFor="time">Start Time:</label>
+                <label htmlFor="time">Start Time</label>
                 <input className='input-schedule-game' type="time" name="time" value={time} onChange={(e) => setTime(e.target.value)}/>
               </div>
               <div className="four wide field">
                 <label htmlFor="privacy">Game Availability</label>
                 <select class="ui dropdown input-schedule-game" name='privacy' value={privacy} onChange={(e) => setPrivacy(e.target.value)}>
-                  <option value="Public">Open to the Public</option>
+                  <option value="Public">Open to Public</option>
                   <option value="Friends">Friends only</option>
                 </select>
               </div>
-              <div className="four wide field">
-                <label htmlFor="numVacancies">Number of Vacancies</label>
+              <div className="three wide field">
+                <label htmlFor="numVacancies">Vacant Spots</label>
                 <select class="ui dropdown input-schedule-game" name='numVacancies' value={numVacancies} onChange={(e) => setNumVacancies(e.target.value)}>
                   <option value="1">1</option>
                   <option value="2">2</option>
